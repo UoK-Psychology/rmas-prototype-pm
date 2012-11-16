1ed0d0790c1a8d2c005298a80b5f09f117e9002b
Jason Marshall <ctrlspc@gmail.com>
2012-11-16 14:24:53 +0000

    changed the proposals model to send valid cerif xml

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -85,15 +85,70 @@ class Proposal < ActiveRecord::Base
 	def send_message(message)
 
 		client = Savon::Client.new do
-			wsdl.document = 'http://129.12.9.208:6980/EventService?wsdl'
+			wsdl.document = 'http://localhost:7789/?wsdl'
 		end
 
 		message['message_id'] = generate_rmas_id()
 
+    a_title = "does this work?"
+
+    xml_message = "<?xml version='1.0' encoding='UTF-8'?> 
+    <rmas>
+    	<message-type>Proposal-created</message-type><!-- RMAS message type -->
+    	<!-- CERIF payload -->
+    	<CERIF
+    		xmlns='urn:xmlns:org:eurocris:cerif-1.4-0' 
+    		xsi:schemaLocation='urn:xmlns:org:eurocris:cerif-1.4-0http://www.eurocris.org/Uploads/Web%20pages/CERIF-1.4/CERIF_1.4_0.xsd' 
+    		xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+    		release='1.4'
+    		date='2012-04-12'
+    		sourceDatabase='pFact'> 
+    		<!-- Base project entity -->
+    		<cfProj>
+    			<cfProjId>urn:rmas:0078:pfact:2.02:UUID</cfProjId> <!-- RMAS identifier --> 
+    			<cfStartDate>2010-01-01</cfStartDate> <!-- Project start --> 
+    			<cfEndDate>2012-07-31</cfEndDate> <!-- Project end --> 
+    			<cfAcro>RMAS</cfAcro> <!-- Project acronym -->
+    			<cfTitle
+    				cfLangCode='EN'
+    				cfTrans='o'>#{self.title}</cfTitle> <!-- Link entity (denoting project co-ordinator) -->
+    			<cfProj_OrgUnit>
+    				<cfOrgUnitId>orgunit-exeter-internal-id</cfOrgUnitId>
+    				<cfClassId>c31d3380-1cfd-11e1-8bc2-0800200c9a66</cfClassId>
+    				<cfClassSchemeId>6b2b7d25-3491-11e1-b86c-0800200c9a66</cfClassSchemeId>
+    			  	<cfStartDate>2010-01-01T00:00:00</cfStartDate> <!-- Project start --> 
+    			  	<cfEndDate>2012-07-31T00:00:00</cfEndDate> <!-- Project end -->
+    			</cfProj_OrgUnit>
+
+    			<cfProj_Pers><!-- Link entity (denoting project principal investigator -->
+    				<cfPersId>1123456</cfPersId>
+    				<cfClassId>b0e11470-1cfd-11e1-8bc2-0800200c9a66</cfClassId>
+    				<cfClassSchemeId>94fefd50-1d00-11e1-8bc2-0800200c9a66</cfClassSchemeId>  
+    				<cfStartDate>2010-01-01T00:00:00</cfStartDate> <!-- Project start --> 
+    				<cfEndDate>2012-07-31T00:00:00</cfEndDate> <!-- Project end -->
+    			</cfProj_Pers> 
+    		</cfProj>
+
+    		<cfOrgUnit><!-- Referenced organisation entity -->
+    			<cfOrgUnitId>orgunit-exeter-internal-id</cfOrgUnitId>
+    			<cfName cfLangCode='en_GB' cfTrans='o'>University of Exeter</cfName>
+    		</cfOrgUnit>
+
+    		<cfPers> <!-- Referenced person entity -->
+    			<cfPersId>1123456</cfPersId> 
+    			<cfGender>m</cfGender>
+    			<cfPersName>
+    				<cfFamilyNames>Marshall</cfFamilyNames>
+    				<cfFirstNames>Jason</cfFirstNames> 
+    			</cfPersName>
+    		</cfPers>
+    	</CERIF>
+    </rmas>"
+  
 		message_json = ActiveSupport::JSON.encode(message)
 
 		response = client.request :push_event do
-			soap.body = { :message => message_json }
+			soap.body = { :event => xml_message }
 		end
 
 		unless response.success?
